bcff376c3f5b03bc206535c8dd4d12c9113af4fc
wpsilva <wederpacheco.s@gmail.com>
2019-12-09 10:09:59 -0300

    Added all apps pages

---
+++ src/pages/no-match/nomatch.component.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./nomatch.style.scss";
+
+const NoMatch: React.FC = () => (
+  <div className="nomatch-box">
+    <h1 className="nomatch-text">No Match</h1>
+  </div>
+);
+
+export default NoMatch;
