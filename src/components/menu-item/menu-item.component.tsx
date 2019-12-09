db89cd8a22e03876a31f6670d5881c43b99b6d77
wpsilva <wederpacheco.s@gmail.com>
2019-12-09 10:12:54 -0300

    MenuItem component added

---
+++ src/components/menu-item/menu-item.component.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./menu-item.styles.scss";
+import { Section } from "../directory/directory.component";
+
+const MenuItem: React.FC<Section> = section => {
+  const { imageUrl, size, title } = section;
+  return (
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className={`${size} menu-item`}
+    >
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
